a5439dd2d03202b549626fc8b183a32281b87f60
luizpsg <999785@sga.pucminas.br>
2024-05-11 22:50:27 -0300

    chore: Update StarRating component with additional props and styles

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -7,6 +7,16 @@ import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating
+      maxRating={5}
+      size={24}
+      color="red"
+      className="test"
+      defaultRating={3}
+    />
   </React.StrictMode>
 );
