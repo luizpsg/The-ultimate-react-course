5de41f4bea7774c58e600e633b3d0f548bda046a
luizpsg <luiz.saud@sga.pucminas.br>
2024-05-16 17:17:37 -0300

    A first look at effects

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,22 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
+// import StarRating from "./StarRating";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <StarRating
+//       maxRating={5}
+//       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+//     />
+//     <StarRating
+//       maxRating={5}
+//       size={24}
+//       color="red"
+//       className="test"
+//       defaultRating={3}
+//     />
+//   </React.StrictMode>
+// );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <StarRating
-      maxRating={5}
-      size={24}
-      color="red"
-      className="test"
-      defaultRating={3}
-    />
+    <App />
   </React.StrictMode>
 );
