57d33979c94cfacc259f8f2b24f8d9437bda7e11
luizpsg <luiz.saud@sga.pucminas.br>
2024-08-15 20:16:45 -0300

    refactor: Add "Restart Quiz" button to FinishScreen component

--- 09-react-quiz/src/components/App.js
+++ 09-react-quiz/src/components/App.js
@@ -68,6 +68,13 @@ function reducer(state, action) {
           state.points > state.highscore ? state.points : state.highscore,
       };
 
+    case "restart":
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready",
+      };
+
     default:
       throw new Error("Invalid action type");
   }
@@ -127,6 +134,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
