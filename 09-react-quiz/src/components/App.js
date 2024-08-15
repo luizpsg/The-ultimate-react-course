118f95c4280f3954cab2ce77a98528cf4495582b
luizpsg <luiz.saud@sga.pucminas.br>
2024-08-15 19:25:38 -0300

    refactor: Update App component to use Question and Options components

--- 09-react-quiz/src/components/App.js
+++ 09-react-quiz/src/components/App.js
@@ -11,6 +11,7 @@ const initialState = {
 
   // loanding, error, ready, active, finished
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -40,7 +41,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
 
@@ -61,7 +65,7 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
